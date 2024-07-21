46c3bdc353f4a7089ab085c0b803aca500ebba15
mirshadalis <coding.irshadali@gmail.com>
2024-07-21 21:58:07 +0530

    Fixed the role updation

--- index.js
+++ index.js
@@ -344,156 +344,157 @@ client.on("messageCreate", async (message) => {
     }
   } else if (command === "p") {
     if (message.channel.id !== botCommandsId && message.channel.parentId != 1252874447359049850) return;
-    const studyRoles = [
-      {
-        name: "Novice Scholar",
-        time: 1, 
-        roleId: "1254721791226810450",
-      },
-      {
-        name: "Apprentice Scholar",
-        time: 3, 
-        roleId: "1254722449300389990",
-      },
-      {
-        name: "Junior Scholar",
-        time: 5,
-        roleId: "1254722592837861426",
-      },
-      {
-        name: "Adept Scholar",
-        time: 10, 
-        roleId: "1254722732470439967",
-      },
-      {
-        name: "Skilled Scholar",
-        time: 15,
-        roleId: "1254722933008629780",
-      },
-      {
-        name: "Seasoned Scholar",
-        time: 20, 
-        roleId: "1254723284969197588",
-      },
-      {
-        name: "Advanced Scholar",
-        time: 30, 
-        roleId: "1254723489387249684",
-      },
-      {
-        name: "Expert Scholar",
-        time: 40,
-        roleId: "1254723566729953342",
-      },
-      {
-        name: "Master Scholar",
-        time: 50, 
-        roleId: "1254723709458059339",
-      },
-      
-      {
-        name: "Senior Scholar",
-        time: 65,
-        roleId: "1254723826034409482",
-      },
-      {
-        name: "Elite Scholar",
-        time: 80, 
-        roleId: "1254724434758209536",
-      },
-      {
-        name: "Prodigious Scholar",
-        time: 100, 
-        roleId: "1254724538659241997",
-      },
-      {
-        name: "Renowned Scholar",
-        time: 125, 
-        roleId: "1254724607014076497",
-      },
-      {
-        name: "Legendary Scholar",
-        time: 150, 
-        roleId: "1254724710885757009",
-      },
-      {
-        name: "Eminent Scholar",
-        time: 9999999, 
-        roleId: "1254724765659172864",
+    try {
+      const studyRoles = [
+        {
+          name: "Novice Scholar",
+          time: 0, 
+          roleId: "1254721791226810450",
+        },
+        {
+          name: "Apprentice Scholar",
+          time: 1, 
+          roleId: "1254722449300389990",
+        },
+        {
+          name: "Junior Scholar",
+          time: 3,
+          roleId: "1254722592837861426",
+        },
+        {
+          name: "Adept Scholar",
+          time: 5, 
+          roleId: "1254722732470439967",
+        },
+        {
+          name: "Skilled Scholar",
+          time: 10,
+          roleId: "1254722933008629780",
+        },
+        {
+          name: "Seasoned Scholar",
+          time: 15, 
+          roleId: "1254723284969197588",
+        },
+        {
+          name: "Advanced Scholar",
+          time: 20, 
+          roleId: "1254723489387249684",
+        },
+        {
+          name: "Expert Scholar",
+          time: 30,
+          roleId: "1254723566729953342",
+        },
+        {
+          name: "Master Scholar",
+          time: 40, 
+          roleId: "1254723709458059339",
+        },
+        
+        {
+          name: "Senior Scholar",
+          time: 50,
+          roleId: "1254723826034409482",
+        },
+        {
+          name: "Elite Scholar",
+          time: 65, 
+          roleId: "1254724434758209536",
+        },
+        {
+          name: "Prodigious Scholar",
+          time: 80, 
+          roleId: "1254724538659241997",
+        },
+        {
+          name: "Renowned Scholar",
+          time: 100, 
+          roleId: "1254724607014076497",
+        },
+        {
+          name: "Legendary Scholar",
+          time: 125, 
+          roleId: "1254724710885757009",
+        },
+        {
+          name: "Eminent Scholar",
+          time: 150, 
+          roleId: "1254724765659172864",
+        }
+      ];
+
+      const userId = message.author.id;
+      const member = message.guild.members.cache.get(userId);
+
+      if (!member) {
+        message.channel.send("Member not found.");
+        return;
       }
-    ];
 
-    const userId = message.author.id;
-    const member = message.guild.members.cache.get(userId);
-
-    if (!member) {
-      message.channel.send("Member not found.");
-      return;
-    }
-
-    db.get(
-      `SELECT total FROM studyTimes WHERE userId = ?`,
-      [userId],
-      (err, row) => {
-        if (err) {
-          console.error("Error fetching data:", err);
-          message.channel.send(
-            "An error occurred while fetching your study time."
-          );
-          return;
-        }
-
-        if (!row) {
-          message.channel.send(
-            "You have not recorded any study time yet."
-          );
-          return;
-        }
-
-        const totalStudyTime = parseFloat(row.total);
-
-        const newRoles = [];
-        const oldRoles = [];
-
-        studyRoles.forEach((role) => {
-          const guildRole = message.guild.roles.cache.get(role.roleId);
-
-          if (guildRole) {
-            if (totalStudyTime >= role.time) {
-              if (!member.roles.cache.has(role.roleId)) {
-                member.roles.add(role.roleId);
-                newRoles.push(guildRole.name);
-              }
-            } else {
-              if (member.roles.cache.has(role.roleId)) {
-                member.roles.remove(role.roleId);
-                oldRoles.push(guildRole.name);
-              }
-            }
-          } else {
-            console.error(`Role ${role.name} not found in the server.`);
+      const row = await new Promise((resolve, reject) => {
+        db.get(
+          `SELECT * FROM studyTimes WHERE userId = ?`,
+          [userId],
+          (err, row) => {
+            if (err) reject(err); 
+            else resolve(row);
           }
-        });
+        );
+      });
 
-        let responseMessage = "Roles updated:\n";
-
-        if (newRoles.length > 0) {
-          responseMessage += `Added: ${newRoles.join(", ")}\n`;
-        }
-
-        if (oldRoles.length > 0) {
-          responseMessage += `Removed: ${oldRoles.join(", ")}`;
-        }
-
-        if (newRoles.length === 0 && oldRoles.length === 0) {
-          responseMessage += "No role changes.";
-        }
-
-        message.channel.send(responseMessage);
+      if (!row) {
+        return message.channel.send("You have not recorded any study time yet.");
       }
-    );
+
+      const monthlyStudyTime = parseFloat(row.monthly);
+      const responseMessage = await updateRoles(member, monthlyStudyTime, studyRoles);
+      message.channel.send(responseMessage);
+    } catch (error) {
+      console.error("Error updating roles:", error);
+      message.channel.send("An error occurred while updating your roles.");
+    }
   }
 });
+
+async function updateRoles(member, totalStudyTime, studyRoles) {
+  const newRoles = [];
+  const removedRoles = []; 
+
+  for (const roleData of studyRoles) {
+    const guildRole = member.guild.roles.cache.get(roleData.roleId);
+
+    if (!guildRole) {
+      console.error(`Role ${roleData.name} not found in the server.`);
+      continue; 
+    }
+
+    if (totalStudyTime >= roleData.time && !member.roles.cache.has(roleData.roleId)) {
+      await member.roles.add(roleData.roleId).catch(console.error); 
+      newRoles.push(guildRole.name);
+    } else if (totalStudyTime < roleData.time && member.roles.cache.has(roleData.roleId)) {
+      await member.roles.remove(roleData.roleId).catch(console.error);
+      removedRoles.push(guildRole.name);
+    }
+  }
+
+  let responseMessage = "Roles updated:\n";
+
+  if (newRoles.length > 0) {
+    responseMessage += `Added: ${newRoles.join(", ")}\n`;
+  }
+
+  if (removedRoles.length > 0) {
+    responseMessage += `Removed: ${removedRoles.join(", ")}\n`;
+  }
+
+  if (newRoles.length === 0 && removedRoles.length === 0) {
+    responseMessage = "No role changes."; 
+  }
+
+  return responseMessage;
+}
+
 
 async function calculateRank(statColumn, userStatValue) {
   try {
